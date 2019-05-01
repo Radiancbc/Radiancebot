57359054cc82bc5a4902b4d94fc3d1731c8267c5
Radiancbc <50171233+Radiancbc@users.noreply.github.com>
2019-05-01 15:20:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const bot = new Discord.Client();
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+
+bot.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 
